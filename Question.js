0d45f4567a6dd67ab3396124e0de046f0eba8fff
Sylvie-B <unsolo@hotmail.fr>
2021-12-23 02:39:43 +0100

    create json

---
+++ Question.js
@@ -0,0 +1,14 @@
+let Question = `{
+    "citation" : [
+        "titre du film",
+        "information",
+        "url"
+        ],
+    "Il va nous falloir un plus grand bateau" : [
+        "Les dents de la mer",
+        "L'équipe de tournage disait cette phrase concernant le budget du film, et le personnage principal la dites 
+        lors d'une improvisation",
+        "url('')"
+        ],
+        
+}`;
