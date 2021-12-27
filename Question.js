c8bd0520250052cd966f648b32032b13cf5a274d
Sylvie-B <unsolo@hotmail.fr>
2021-12-27 14:00:23 +0100

    suppr citation []

--- Question.js
+++ Question.js
@@ -3,186 +3,118 @@ let question = [
     "title": "Captaine America",
     "link": "url:('')",
     "citation":
-      [
         "Je pourrais faire ça toute la journée"
-      ]
   },
   {
     "title": "Forest Gump",
     "link": "url:('')",
     "citation":
-      [
         "La vie c'est comme une boîte de chocolat"
-      ]
   },
   {
     "title": "Hunger games",
     "link": "url:('')",
-    "citation":
-        [
-          "Puisse le sort vous être favorable"
-        ]
+    "citation": "Puisse le sort vous être favorable"
   },
   {
     "title": "Knight and day",
     "link": "url:('')",
-    "citation":
-        [
-          "Avec moi... Sans moi. Avec moi... sans moi"
-        ]
+    "citation": "Avec moi... Sans moi. Avec moi... sans moi"
   },
   {
     "title": "Les gardiens de la galaxie",
     "link": "url:('')",
-    "citation":
-        [
-          "Je s'apelle Groot"
-        ]
+    "citation": "Je s'apelle Groot"
   },
   {
     "title": "Le cinquième élèment",
     "link": "url:('')",
-    "citation":
-        [
-          "Aziz lumière !"
-        ]
+    "citation": "Aziz lumière !"
+
   },
   {
     "title": "Point break",
     "link": "url:('')",
-    "citation":
-        [
-          "J'ai pris une déferlante d'enfer ce matin"
-        ]
+    "citation": "J'ai pris une déferlante d'enfer ce matin"
   },
   {
     "title": "Road house",
     "link": "url:('')",
-    "citation":
-        [
-          "J'vous aurez cru plus balaise"
-        ]
+    "citation":"J'vous aurez cru plus balaise"
   },
   {
     "title": "Brice de Nice",
     "link": "url:('')",
-    "citation":
-        [
-          "Tout est dans Point Break"
-        ]
+    "citation": "Tout est dans Point Break"
   },
   {
     "title": "Rrrr",
     "link": "url:('')",
-    "citation":
-        [
-          "Y va faire tout noir"
-        ]
+    "citation": "Y va faire tout noir"
   },
   {
     "title": "Piège de cristal",
     "link": "url:('')",
-    "citation":
-        [
-          "Bienvenue à la fête camarade !"
-        ]
+    "citation": "Bienvenue à la fête camarade !"
   },
   {
   "title" : "Matrix",
   "link" : "url:('img/matrix.png')",
-  "citation" :
-    [
-      // "On est pas le meilleur quand on le croit mais quand on le sait.",
-      // "La cuillère n'existe pas",
-      "Pourquoi j'ai pas pris la pillule bleue ?"
-    ]
+  "citation" : "Pourquoi j'ai pas pris la pillule bleue ?"
   },
   {
     "title": "Le cercle des poètes disparus",
     "link": "url:('')",
-    "citation":
-        [
-          "Oh Capitaine, mon capitaine !"
-        ]
+    "citation": "Oh Capitaine, mon capitaine !"
   },
   {
     "title": "Usual suspect",
     "link": "url:('')",
-    "citation":
-        [
-          "Le coup le plus rusé que le diable ait jamais réussi, ça a été de faire à tout le monde qu'il n'existe pas"
-        ]
+    "citation": "Le coup le plus rusé que le diable ait jamais réussi, ça a été de faire à tout le monde qu'il n'existe pas"
   },
   {
     "title": "Spiderman",
     "link": "url:('')",
-    "citation":
-    [
-      "Un grand pouvoir implique de grandes responsabilités"
-    ]
+    "citation": "Un grand pouvoir implique de grandes responsabilités"
   },
   {
     "title":"Star wars VII",
     "link": "url:('')",
-    "citation":
-    [
-      "Je suis ton père"
-    ]
+    "citation": "Je suis ton père"
   },
   {
     "title": "La communauté de l'anneau",
     "link": "url:('')",
-    "citation":
-    [
-      "Vous ne passerez pas !"
-    ]
+    "citation": "Vous ne passerez pas !"
   },
   {
     "title": "E.T.",
     "link": "url:('')",
-    "citation":
-    [
-      "Téléphone maison"
-    ]
+    "citation": "Téléphone maison"
   },
   {
     "title": "Kaamelott",
     "link": "url:('')",
-    "citation":
-    [
-      "C'est pas faut !"
-    ]
+    "citation": "C'est pas faut !"
   },
   {
     "title": "Dirty dancing",
     "link": "url:('')",
-    "citation":
-    [
-      "On laisse pas bébé dans un coin"
-    ]
+    "citation": "On laisse pas bébé dans un coin"
   },
   {
     "title": "Les dents de la mer",
     "link": "url:('')",
-    "citation":
-    [
-      "Il va nous falloir un plus grand bateau",
-    ]
+    "citation": "Il va nous falloir un plus grand bateau",
   },
   {
     "title": "Terminator II",
     "link": "url:('')",
-    "citation":
-    [
-      "Je reviendrais"
-    ]
+    "citation": "Je reviendrais"
   },
   {
     "title": "Piège en haute mer",
     "link": "url:('')",
-    "citation":
-    [
-      "L'endroit le plus sûr sur ce bateau, c'est derrière vous"
-    ]
+    "citation": "L'endroit le plus sûr sur ce bateau, c'est derrière vous"
   }
 ]
